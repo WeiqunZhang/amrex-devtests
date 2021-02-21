3cd09ae50997fbc14725fabf0da152b60b93620d
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-02-21 11:17:32 -0800

    solve_2d_in_3d: add plotfile

--- solve_2d_in_3d/MyTestPlotfile.cpp
+++ solve_2d_in_3d/MyTestPlotfile.cpp
@@ -8,70 +8,31 @@ using namespace amrex;
 void
 MyTest::writePlotfile () const
 {
-#if 0
-    ParmParse pp;
-    bool gpu_regtest = false;
-#ifdef AMREX_USE_GPU
-    pp.query("gpu_regtest", gpu_regtest);
-#endif
-
-    const int nlevels = max_level+1;
-    Vector<MultiFab> plotmf(nlevels);
-
-    if (gpu_regtest) {
-        const int ncomp = (acoef.empty()) ? 3 : 5;
-        Vector<std::string> varname = {"solution", "rhs", "exact_solution"};
-        if (!acoef.empty()) {
-            varname.emplace_back("acoef");
-            varname.emplace_back("bcoef");
-        }
-
-        for (int ilev = 0; ilev < nlevels; ++ilev)
-        {
-            plotmf[ilev].define(grids[ilev], dmap[ilev], ncomp, 0);
-            MultiFab::Copy(plotmf[ilev], solution      [ilev], 0, 0, 1, 0);
-            MultiFab::Copy(plotmf[ilev], rhs           [ilev], 0, 1, 1, 0);
-            MultiFab::Copy(plotmf[ilev], exact_solution[ilev], 0, 2, 1, 0);
-            if (!acoef.empty()) {
-                MultiFab::Copy(plotmf[ilev], acoef[ilev], 0, 3, 1, 0);
-                MultiFab::Copy(plotmf[ilev], bcoef[ilev], 0, 4, 1, 0);
-            }
-        }
-
-        WriteMultiLevelPlotfile("plot", nlevels, amrex::GetVecOfConstPtrs(plotmf),
-                                varname, geom, 0.0, Vector<int>(nlevels, 0),
-                                Vector<IntVect>(nlevels, IntVect{ref_ratio}));
-    } else {
-        const int ncomp = (acoef.empty()) ? 4 : 6;
-        Vector<std::string> varname = {"solution", "rhs", "exact_solution", "error"};
-        if (!acoef.empty()) {
-            varname.emplace_back("acoef");
-            varname.emplace_back("bcoef");
-        }
-
-        for (int ilev = 0; ilev < nlevels; ++ilev)
-        {
-            plotmf[ilev].define(grids[ilev], dmap[ilev], ncomp, 0);
-            MultiFab::Copy(plotmf[ilev], solution      [ilev], 0, 0, 1, 0);
-            MultiFab::Copy(plotmf[ilev], rhs           [ilev], 0, 1, 1, 0);
-            MultiFab::Copy(plotmf[ilev], exact_solution[ilev], 0, 2, 1, 0);
-            MultiFab::Copy(plotmf[ilev], solution      [ilev], 0, 3, 1, 0);
-            MultiFab::Subtract(plotmf[ilev], plotmf[ilev], 2, 3, 1, 0); // error = soln - exact
-            if (!acoef.empty()) {
-                MultiFab::Copy(plotmf[ilev], acoef[ilev], 0, 4, 1, 0);
-                MultiFab::Copy(plotmf[ilev], bcoef[ilev], 0, 5, 1, 0);
-            }
-            auto dx = geom[ilev].CellSize();
-            Real dvol = AMREX_D_TERM(dx[0],*dx[1],*dx[2]);
-            amrex::Print() << "Level " << ilev
-                           << " max-norm error: " << plotmf[ilev].norminf(3)
-                           << " 1-norm error: " << plotmf[ilev].norm1(3)*dvol << std::endl;
-        }
-
-        WriteMultiLevelPlotfile("plot", nlevels, amrex::GetVecOfConstPtrs(plotmf),
-                                varname, geom, 0.0, Vector<int>(nlevels, 0),
-                                Vector<IntVect>(nlevels, IntVect{ref_ratio}));
+    Vector<std::string> varname = {"solution", "rhs", "exact_solution", "error"};
+    int ncomp = varname.size();
+    if (!acoef.empty()) {
+        varname.emplace_back("acoef");
+        ++ncomp;
     }
-#endif
+    if (!bcoef.empty()) {
+        varname.emplace_back("bcoef");
+        ++ncomp;
+    }
+
+    MultiFab plotmf(grids, dmap, ncomp, 0);
+    MultiFab::Copy(plotmf, solution      , 0, 0, 1, 0);
+    MultiFab::Copy(plotmf, rhs           , 0, 1, 1, 0);
+    MultiFab::Copy(plotmf, exact_solution, 0, 2, 1, 0);
+    MultiFab::Copy(plotmf, solution      , 0, 3, 1, 0);
+    MultiFab::Subtract(plotmf, plotmf, 2, 3, 1, 0); // error = soln - exact
+    if (!acoef.empty()) {
+        MultiFab::Copy(plotmf, acoef, 0, 4, 1, 0);
+    }
+    if (!bcoef.empty()) {
+        MultiFab::Copy(plotmf, bcoef, 0, 5, 1, 0);
+    }
+    amrex::Print() << " max-norm error: " << plotmf.norminf(3) << std::endl;
+
+    WriteSingleLevelPlotfile("plot", plotmf, varname, geom, 0.0, 0);
 }
 
