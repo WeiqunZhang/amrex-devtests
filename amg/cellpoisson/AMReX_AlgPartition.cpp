6daeef41a2231dff81522ec0e884e9117b342e20
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-04-28 17:13:51 -0700

    build partition

--- amg/cellpoisson/AMReX_AlgPartition.cpp
+++ amg/cellpoisson/AMReX_AlgPartition.cpp
@@ -2,4 +2,29 @@
 
 namespace amrex {
 
+AlgPartition::AlgPartition ()
+    : m_ref(std::make_shared<Ref>())
+{}
+
+AlgPartition::AlgPartition (Vector<Long> const& rows)
+    : m_ref(std::make_shared<Ref>(rows))
+{}
+
+AlgPartition::AlgPartition (Vector<Long>&& rows)
+    : m_ref(std::make_shared<Ref>(std::move(rows)))
+{}
+
+AlgPartition::~AlgPartition ()
+{}
+
+void AlgPartition::define (Vector<Long> const& rows)
+{
+    m_ref->m_row = rows;
+}
+
+void AlgPartition::define (Vector<Long>&& rows)
+{
+    m_ref->m_row = std::move(rows);
+}
+
 }
--- amg/cellpoisson/AMReX_AlgRow.cpp
+++ amg/cellpoisson/AMReX_AlgRow.cpp
@@ -1,4 +1,10 @@
 #include <AMReX_AlgRow.H>
+#include <AMReX_LayoutData.H>
+#include <AMReX_Vector.H>
+#include <AMReX_ParallelContext.H>
+
+#include <limits>
+#include <numeric>
 
 namespace amrex {
 
@@ -18,6 +24,24 @@ void AlgRow::define (BoxArray const& ba, DistributionMapping const& dm,
     m_geom = geom;
     m_id.define(ba, dm, 1, nghost);
 
+    Vector<Long> ncells_allprocs(ParallelDescriptor::NProcs(), 0);
+    for (int k = 0, N = ba.size(); k < N; ++k) {
+        ncells_allprocs[dm[k]] += ba[k].numPts();
+    }
+
+    Vector<Long> rows(ncells_allprocs.size()+1);
+    rows[0] = 0;
+    std::partial_sum(ncells_allprocs.begin(), ncells_allprocs.end(), rows.begin()+1);
+
+    m_partition.define(std::move(rows));
+
+    Long id_begin = m_partition[ParallelDescriptor::MyProc()];
+    for (MFIter mfi(m_id); mfi.isValid(); ++mfi) {
+        Box const& vbx = mfi.validbox();
+        Box const& fbx = mfi.fabbox();
+        // parallel for to fill the data including ghost cells setting to lowest
+        id_begin += vbx.numPts();
+    }
 }
 
 const FabArray<BaseFab<Long> >&
