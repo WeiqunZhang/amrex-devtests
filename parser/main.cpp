1ba06646d9f2d2532304e70a270db1e41935600b
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-06-20 07:28:51 -0700

    more test

--- parser/main.cpp
+++ parser/main.cpp
@@ -26,6 +26,9 @@ int main (int argc, char* argv[])
         test("if(z<zp, nc*exp((z-zc)/lgrad), if(z<=zp2, 2.*nc, nc*exp(-(z-zc2)/lgrad)))",
              {{"zp", 1.0}, {"nc", 1.e-6}, {"zc", 0.6}, {"lgrad", 2.5}, {"zp2", 2.0}, {"zc2", 1.3}},
              {"z"});
+        test("3.*2.*x*.4*0.5*sin(y)*2.*3.*z*4.*0.5",
+             {},
+             {"x","y","z"});
     }
     amrex::Print() << "\n";
 
