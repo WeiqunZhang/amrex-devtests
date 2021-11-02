41c9dbd65e9ef35de1e2ee7640cd8575a2b06ce6
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-11-02 15:18:04 -0700

    do not use managed memory

--- gatherandpush/EMParticleContainerInit.cpp
+++ gatherandpush/EMParticleContainerInit.cpp
@@ -68,10 +68,10 @@ InitParticles(const IntVect& a_num_particles_per_cell,
         const auto lo = amrex::lbound(tile_box);
         const auto hi = amrex::ubound(tile_box);
 
-        Gpu::ManagedVector<unsigned int> counts(tile_box.numPts(), 0);
+        Gpu::DeviceVector<unsigned int> counts(tile_box.numPts(), 0);
         unsigned int* pcount = counts.dataPtr();
 
-        Gpu::ManagedVector<unsigned int> offsets(tile_box.numPts());
+        Gpu::DeviceVector<unsigned int> offsets(tile_box.numPts());
         unsigned int* poffset = offsets.dataPtr();
 
         amrex::ParallelFor(tile_box,
@@ -105,9 +105,7 @@ InitParticles(const IntVect& a_num_particles_per_cell,
             }
         });
 
-        Gpu::exclusive_scan(counts.begin(), counts.end(), offsets.begin());
-
-        int num_to_add = offsets[tile_box.numPts()-1] + counts[tile_box.numPts()-1];
+        auto num_to_add = Scan::ExclusiveSum(counts.size(), pcount, poffset);
 
         auto& particles = GetParticles(lev);
         auto& particle_tile = particles[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
