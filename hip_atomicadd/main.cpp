0b2f896cf98f0990fd086088fc72964edbba46c7
Weiqun Zhang <weiqunzhang@lbl.gov>
2022-06-13 18:52:00 -0400

    more on hip atomicadd test

--- hip_atomicadd/main.cpp
+++ hip_atomicadd/main.cpp
@@ -7,6 +7,8 @@
 
 using namespace amrex;
 
+//#define ATOMIC_ADD_SHARED
+
 int main (int argc, char* argv[])
 {
     amrex::Initialize(argc,argv);
@@ -28,42 +30,128 @@ int main (int argc, char* argv[])
             a(i,j,k) = Random(engine);
             if (i == 0 && j == 0 && k == 0) { *dp = Real(0.); }
         });
-        Gpu::synchronize();
+        Gpu::streamSynchronize();
 
-        double t_atomicadd, t_atomicaddnoret;
+        double t_atomicadd, t_atomicaddnoret, t_unsafeatomicadd, t_amrexatomicadd;
 
         for (int i = 0; i < 2; ++i) {
+            amrex::single_task([=] AMREX_GPU_DEVICE () { *dp = 0._rt; });
+            Gpu::streamSynchronize();
             double ttmp = amrex::second();
             amrex::ParallelFor(Gpu::KernelInfo().setReduction(true), box,
             [=] AMREX_GPU_DEVICE (int i, int j, int k, Gpu::Handler const& gh)
             {
+#ifdef ATOMIC_ADD_SHARED
+                __shared__ Real r;
+                if (threadIdx.x == 0) { r = 0._rt; }
+                __syncthreads();
+                atomicAdd(&r, a(i,j,k));
+                __syncthreads();
+                if (threadIdx.x == 0) { atomicAdd(dp,r); }
+#else
                 if (gh.isFullBlock()) {
                     Real r = Gpu::blockReduceSum<AMREX_GPU_MAX_THREADS>(a(i,j,k));
                     if (threadIdx.x == 0) { atomicAdd(dp,r); }
                 }
+#endif
             });
-            Gpu::synchronize();
+            Gpu::streamSynchronize();
             t_atomicadd = amrex::second()-ttmp;
+            Real result;
+            Gpu::dtoh_memcpy(&result, dp, sizeof(Real));
+            amrex::Print() << "atomicAdd Result is " << result << std::endl;
         }
 
         for (int i = 0; i < 2; ++i) {
+            amrex::single_task([=] AMREX_GPU_DEVICE () { *dp = 0._rt; });
+            Gpu::streamSynchronize();
             double ttmp = amrex::second();
             amrex::ParallelFor(Gpu::KernelInfo().setReduction(true), box,
             [=] AMREX_GPU_DEVICE (int i, int j, int k, Gpu::Handler const& gh)
             {
+#ifdef ATOMIC_ADD_SHARED
+                __shared__ Real r;
+                if (threadIdx.x == 0) { r = 0._rt; }
+                __syncthreads();
+                atomicAddNoRet(&r, a(i,j,k));
+                __syncthreads();
+                if (threadIdx.x == 0) { atomicAddNoRet(dp,r); }
+#else
                 if (gh.isFullBlock()) {
                     Real r = Gpu::blockReduceSum<AMREX_GPU_MAX_THREADS>(a(i,j,k));
                     if (threadIdx.x == 0) { atomicAddNoRet(dp,r); }
                 }
+#endif
             });
-            Gpu::synchronize();
+            Gpu::streamSynchronize();
             t_atomicaddnoret = amrex::second()-ttmp;
+            Real result;
+            Gpu::dtoh_memcpy(&result, dp, sizeof(Real));
+            amrex::Print() << "atomicAddNoRet result is " << result << std::endl;
+        }
+
+        for (int i = 0; i < 2; ++i) {
+            amrex::single_task([=] AMREX_GPU_DEVICE () { *dp = 0._rt; });
+            Gpu::streamSynchronize();
+            double ttmp = amrex::second();
+            amrex::ParallelFor(Gpu::KernelInfo().setReduction(true), box,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k, Gpu::Handler const& gh)
+            {
+#ifdef ATOMIC_ADD_SHARED
+                __shared__ Real r;
+                if (threadIdx.x == 0) { r = 0._rt; }
+                __syncthreads();
+                unsafeAtomicAdd(&r, a(i,j,k));
+                __syncthreads();
+                if (threadIdx.x == 0) { unsafeAtomicAdd(dp,r); }
+#else
+                if (gh.isFullBlock()) {
+                    Real r = Gpu::blockReduceSum<AMREX_GPU_MAX_THREADS>(a(i,j,k));
+                    if (threadIdx.x == 0) { unsafeAtomicAdd(dp,r); }
+                }
+#endif
+            });
+            Gpu::streamSynchronize();
+            t_unsafeatomicadd = amrex::second()-ttmp;
+            Real result;
+            Gpu::dtoh_memcpy(&result, dp, sizeof(Real));
+            amrex::Print() << "unsafeAtomicAdd result is " << result << std::endl;
+        }
+
+        for (int i = 0; i < 2; ++i) {
+            amrex::single_task([=] AMREX_GPU_DEVICE () { *dp = 0._rt; });
+            Gpu::streamSynchronize();
+            double ttmp = amrex::second();
+            amrex::ParallelFor(Gpu::KernelInfo().setReduction(true), box,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k, Gpu::Handler const& gh)
+            {
+#ifdef ATOMIC_ADD_SHARED
+                __shared__ Real r;
+                if (threadIdx.x == 0) { r = 0._rt; }
+                __syncthreads();
+                Gpu::Atomic::Add(&r, a(i,j,k));
+                __syncthreads();
+                if (threadIdx.x == 0) { Gpu::Atomic::Add(dp,r); }
+#else
+                if (gh.isFullBlock()) {
+                    Real r = Gpu::blockReduceSum<AMREX_GPU_MAX_THREADS>(a(i,j,k));
+                    if (threadIdx.x == 0) { Gpu::Atomic::Add(dp,r); }
+                }
+#endif
+            });
+            Gpu::streamSynchronize();
+            t_amrexatomicadd = amrex::second()-ttmp;
+            Real result;
+            Gpu::dtoh_memcpy(&result, dp, sizeof(Real));
+            amrex::Print() << "amrexatomicadd result is " << result << std::endl;
         }
 
         amrex::Print() << std::scientific << "atomicAdd time: " << t_atomicadd
-                       << ", atomicAddNoRet time: " << t_atomicaddnoret << std::endl;
+                       << ", atomicAddNoRet time: " << t_atomicaddnoret
+                       << ", unsafeAtomicAdd time: " << t_unsafeatomicadd
+                       << ", amrexatomicadd time: " << t_amrexatomicadd << std::endl;
 
-        Gpu::synchronize();
+        Gpu::streamSynchronize();
         The_Arena()->free(dp);
     }
     amrex::Finalize();
