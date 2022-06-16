e21bc9cbc981def3a60b7dfcc816245b78163adb
Weiqun Zhang <weiqunzhang@lbl.gov>
2022-06-16 11:49:41 -0700

    modify parallefor_mf test

--- parallelfor_mf/main.cpp
+++ parallelfor_mf/main.cpp
@@ -6,48 +6,59 @@ using namespace amrex;
 
 void test_setval (MultiFab& mf)
 {
-    double t_orig = 0., t_new = 0.;
+    double t_multli = 0., t_single = 0.;
     for (int itest = 0; itest < 2; ++itest) {
         double t0 = amrex::second();
-        mf.setVal(0.33);
+
+        for (MFIter mfi(mf); mfi.isValid(); ++mfi) {
+            auto const& a = mf.array(mfi);
+            amrex::ParallelFor(mfi.validbox(), [=] AMREX_GPU_DEVICE (int i, int j, int k)
+            {
+                a(i,j,k) = 1.0;
+            });
+        }
+
         double t1 = amrex::second();
+
         {
             auto ma = mf.arrays();
-            amrex::experimental::ParallelFor(mf,
+            amrex::ParallelFor(mf,
             [=] AMREX_GPU_DEVICE (int box_no, int i, int j, int k)
             {
                 ma[box_no](i,j,k) = 1.0;
             });
             Gpu::synchronize();
         }
+
         double t2 = amrex::second();
-        t_orig = t1-t0;
-        t_new = t2-t1;
+
+        t_multli = t1-t0;
+        t_single = t2-t1;
     }
 
-    if (mf.min(0) != 1.0 || mf.max(0) != 1.0) {
-        amrex::Print() << "ERROR!!! setVal failed "
-                       << mf.min(0) << ", " << mf.max(0) << std::endl;
-    }
+    amrex::Print() << "Run time with " << mf.size() << " kernels is "
+                   << std::scientific << t_multli << "\n"
+                   << "Run time with fused kernel launch is "
+                   << std::scientific << t_single << std::endl;;
 
-    amrex::Print() << "    Kernel run time is " << std::scientific << t_orig
-                   << " " << t_new << ".\n";
 }
 
 int main(int argc, char* argv[])
 {
     amrex::Initialize(argc,argv);
+#ifdef AMREX_USE_MPI
+    static_assert(false, "This is a serial test.");
+#endif
     {
         BL_PROFILE("main()");
 
         int ncell = 256;
-        int max_grid_size;
+        int max_grid_size = 64;
         std::vector<int> box_sizes;
         std::vector<int> nboxes;
         {
             ParmParse pp;
             pp.query("ncell", ncell);
-            max_grid_size = ncell;
             pp.query("max_grid_size", max_grid_size);
             pp.queryarr("box_sizes", box_sizes);
             nboxes.resize(box_sizes.size(),1);
