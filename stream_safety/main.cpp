38d1e36641f8f19dedb772a3c81fa96286a346d7
Weiqun Zhang <weiqunzhang@lbl.gov>
2022-12-09 08:34:46 -0800

    update stream_safety test

--- stream_safety/main.cpp
+++ stream_safety/main.cpp
@@ -19,6 +19,9 @@ int main(int argc, char* argv[])
             ds.assign(1, 2);
             hs.assign(1, 4); // assign is synchronous.
 
+            auto pv = dv.data();
+            auto ps = ds.data();
+
             // expensive copy on stream 0
             Gpu::copyAsync(Gpu::hostToDevice, hv.begin(), hv.end(), dv.begin());
 
@@ -26,16 +29,14 @@ int main(int argc, char* argv[])
             Gpu::copyAsync(Gpu::hostToDevice, hs.begin(), hs.end(), ds.begin());
 
             // a kernel stream 0
-            auto pv = dv.data();
             amrex::single_task(Gpu::gpuStream(), [=] AMREX_GPU_DEVICE () {
-                pv[size-1] += 5;
+                pv[size-1] += ps[0];
             });
 
             // kernel on stream 1 that depends on
             Gpu::Device::setStreamIndex(1);
-            auto p = ds.data();
             amrex::single_task(Gpu::gpuStream(), [=] AMREX_GPU_DEVICE () {
-                *p += 10;
+                *ps += 10;
             });
 
             Gpu::streamSynchronizeAll();
@@ -43,9 +44,11 @@ int main(int argc, char* argv[])
             Gpu::copyAsync(Gpu::deviceToHost, ds.begin(), ds.end(), hs.begin());
             Gpu::streamSynchronize();
             if (hs[0] == 4) {
-                amrex::Print() << "We have a problem\n";
-            } else if (hs[1] != 14) {
-                amrex::Print() << "How did this happen?\n";
+                amrex::Print() << "i = " << i << "\n";
+                amrex::Abort("We have a problem");
+            } else if (hs[0] != 14) {
+                amrex::Print() << "i = " << i << ", hs[0] = " << hs[0] << "\n";
+                amrex::Abort("How did this happen?");
             }
         }
     }
