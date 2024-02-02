5a8fedb98f6652bd82b40c673c8faae810a81ecb
Weiqun Zhang <weiqunzhang@lbl.gov>
2024-02-01 19:49:24 -0800

    update parfor_long

--- parfor_long/main.cpp
+++ parfor_long/main.cpp
@@ -51,6 +51,27 @@ void fnew (FArrayBox& fab)
     });
 }
 
+void ffast (FArrayBox& fab)
+{
+    auto const& a = fab.array();
+    Box const& box = fab.box();
+    const auto ncells = std::uint64_t(box.numPts());
+    const auto lo  = amrex::lbound(box);
+    const auto len = amrex::length(box);
+    Math::FastDivmodU64 fdxy(std::uint64_t(len.x)*std::uint64_t(len.y));
+    Math::FastDivmodU64 fdx (std::uint64_t(len.x));
+    const auto ec = Gpu::makeExecutionConfig<256>(ncells);
+    AMREX_LAUNCH_KERNEL(256, ec.numBlocks, ec.numThreads, 0, Gpu::gpuStream(),
+    [=] AMREX_GPU_DEVICE () noexcept {
+        for (std::uint64_t icell = blockDim.x*blockIdx.x+threadIdx.x, stride = blockDim.x*gridDim.x;
+             icell < ncells; icell += stride)
+        {
+            auto [i, j, k] = detail::get_3d_indice(icell, fdxy, fdx);
+            a(int(i)+lo.x, int(j)+lo.y, int(k)+lo.z) = 3.;
+        }
+    });
+}
+
 int main(int argc, char* argv[])
 {
     amrex::Initialize(argc,argv);
@@ -59,6 +80,7 @@ int main(int argc, char* argv[])
         FArrayBox fab(box);
         fold(fab);
         fnew(fab);
+        ffast(fab);
     }
     amrex::Finalize();
 }
