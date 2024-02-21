f64e74a9e14e173c53be72ded4e96f140953bec2
Weiqun Zhang <weiqunzhang@lbl.gov>
2024-02-21 14:49:06 -0800

    lusolver test

---
+++ lusolver/main.cpp
@@ -0,0 +1,103 @@
+#include <AMReX.H>
+#include <AMReX_Print.H>
+#include <AMReX_LUSolver.H>
+
+using namespace amrex;
+
+int main(int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+    {
+        LUSolver<6,Real> lusolver;
+        Real alpha = 1.0;
+        Real dx = 0.1;
+        Real dy = 0.1;
+        Real dz = 0.1;
+        Real dxx = 1./(dx*dx);
+        Real dyy = 1./(dy*dy);
+        Real dzz = 1./(dz*dz);
+        Real dxy = 1./(dx*dy);
+        Real dxz = 1./(dx*dz);
+        Real dyz = 1./(dy*dz);
+        Real beta = alpha/(dx*dx) * 1.0e-3;
+
+        Array2D<Real,0,5,0,5,Order::C> A
+            {alpha*(dyy+dzz)*2.0 + beta,
+             0.0,
+             -alpha*dxy,
+             alpha*dxy,
+             -alpha*dxz,
+             alpha*dxz,
+             //
+             0.0,
+             alpha*(dyy+dzz)*2 + beta,
+             alpha*dxy,
+             -alpha*dxy,
+             alpha*dxz,
+             -alpha*dxz,
+             //
+             -alpha*dxy,
+             alpha*dxy,
+             alpha*(dxx+dzz)*2 + beta,
+             0.0,
+             -alpha*dyz,
+             alpha*dyz,
+             //
+             alpha*dxy,
+             -alpha*dxy,
+             0.0,
+             alpha*(dxx+dzz)*2 + beta,
+             alpha*dyz,
+             -alpha*dyz,
+             //
+             -alpha*dxz,
+             alpha*dxz,
+             -alpha*dyz,
+             alpha*dyz,
+             alpha*(dxx+dyy)*2 + beta,
+             0.0,
+             alpha*dxz,
+             -alpha*dxz,
+             alpha*dyz,
+             -alpha*dyz,
+             0.0,
+             alpha*(dxx+dyy)*2 + beta};
+
+        lusolver.define(A);
+
+        Array<Real,6> b{1.0,-1.0,2.0,-3.0,4.0,-5.0};
+        Array<Real,6> x;
+        lusolver(x.data(), b.data());
+
+        amrex::Print() << "Matrix:\n";
+        for (int i = 0; i < 6; ++i) {
+            for (int j = 0; j < 6; ++j) {
+                std::cout << "   " << A(i,j) << ", ";
+            }
+            std::cout << std::endl;
+        }
+
+        amrex::Print() << "b:";
+        for (int j = 0; j < 6; ++j) {
+            amrex::Print() << "   " << b[j] << ", ";
+        }
+        amrex::Print() << std::endl;
+
+        amrex::Print() << "x:";
+        for (int j = 0; j < 6; ++j) {
+            amrex::Print() << "   " << x[j] << ", ";
+        }
+        amrex::Print() << std::endl;
+
+        amrex::Print() << "A*x: ";
+        for (int i = 0; i < 6; ++i) {
+            Real r = 0.0;
+            for (int j = 0; j < 6; ++j) {
+                r += A(i,j) * x[j];
+            }
+            amrex::Print() << "   " << r << ", ";
+        }
+        amrex::Print() <<  std::endl;
+    }
+    amrex::Finalize();
+}
