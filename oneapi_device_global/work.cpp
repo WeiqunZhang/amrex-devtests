d52159a6edbdd58647319b17d0c20dace34e87c4
Weiqun Zhang <weiqunzhang@lbl.gov>
2024-07-31 22:13:48 +0000

    minor

--- oneapi_device_global/work.cpp
+++ oneapi_device_global/work.cpp
@@ -5,8 +5,10 @@ void work ()
 {
     amrex::ParallelFor(1, [=] AMREX_GPU_DEVICE (int)
     {
+        amrex::Real x = dg_x;
+        auto xx = x + dg_x;
         AMREX_DEVICE_PRINTF("x = %g, y = %g, %g, %g, %g\n",
-                            dg_x.get(), dg_y[0], dg_y[1], dg_y[2], dg_y[3]);
+                            xx, dg_y[0], dg_y[1], dg_y[2], dg_y[3]);
     });
     amrex::Gpu::streamSynchronize();
 }
