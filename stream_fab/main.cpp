fb46bf88b724c6f604b6796cc2ecb5dfc986c458
Weiqun Zhang <weiqunzhang@lbl.gov>
2024-08-09 19:20:13 -0700

    add Fortran stream tests

--- stream_fab/main.cpp
+++ stream_fab/main.cpp
@@ -8,13 +8,57 @@
 
 using namespace amrex;
 
+extern "C"
+{
+    void fort_stream_copy (Real const*, const int*, const int*,
+                           Real*, const int*, const int*);
+
+    void fort_stream_scale (Real*, const int*, const int*,
+                            Real const*, const int*, const int*, Real);
+
+    void fort_stream_add (Real const*, const int*, const int*,
+                          Real const*, const int*, const int*,
+                          Real*, const int*, const int*);
+
+    void fort_stream_triad (Real*, const int*, const int*,
+                            Real const*, const int*, const int*,
+                            Real const*, const int*, const int*, Real);
+}
+
+void stream_copy_f (FArrayBox const& av, FArrayBox& cv)
+{
+    fort_stream_copy(av.dataPtr(), av.loVect(), av.hiVect(),
+                     cv.dataPtr(), cv.loVect(), cv.hiVect());
+}
+
+void stream_scale_f (FArrayBox& bv, FArrayBox const& cv, Real scalar)
+{
+    fort_stream_scale(bv.dataPtr(), bv.loVect(), bv.hiVect(),
+                      cv.dataPtr(), cv.loVect(), cv.hiVect(), scalar);
+}
+
+void stream_add_f (FArrayBox const& av, FArrayBox const& bv, FArrayBox& cv)
+{
+    fort_stream_add(av.dataPtr(), av.loVect(), av.hiVect(),
+                    bv.dataPtr(), bv.loVect(), bv.hiVect(),
+                    cv.dataPtr(), cv.loVect(), cv.hiVect());
+}
+
+void stream_triad_f (FArrayBox& av, FArrayBox const& bv, FArrayBox const& cv,
+                     Real scalar)
+{
+    fort_stream_triad(av.dataPtr(), av.loVect(), av.hiVect(),
+                      bv.dataPtr(), bv.loVect(), bv.hiVect(),
+                      cv.dataPtr(), cv.loVect(), cv.hiVect(), scalar);
+}
+
 void stream_init (FArrayBox& av, FArrayBox& bv, FArrayBox& cv)
 {
     Box const& box = av.box();
     auto const& a = av.array();
     auto const& b = bv.array();
     auto const& c = cv.array();
-    amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+    amrex::LoopConcurrent(box, [&] AMREX_GPU_DEVICE (int i, int j, int k)
     {
         a(i,j,k) = 1.0;
         b(i,j,k) = 2.0;
@@ -27,7 +71,7 @@ void stream_copy (FArrayBox const& av, FArrayBox& cv)
     Box const& box = av.box();
     auto const& a = av.array();
     auto const& c = cv.array();
-    amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+    amrex::LoopConcurrent(box, [&] AMREX_GPU_DEVICE (int i, int j, int k)
     {
         c(i,j,k) = a(i,j,k);
     });
@@ -38,7 +82,7 @@ void stream_scale (FArrayBox& bv, FArrayBox const& cv, Real scalar)
     Box const& box = bv.box();
     auto const& b = bv.array();
     auto const& c = cv.array();
-    amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+    amrex::LoopConcurrent(box, [&] AMREX_GPU_DEVICE (int i, int j, int k)
     {
         b(i,j,k) = scalar*c(i,j,k);
     });
@@ -50,7 +94,7 @@ void stream_add (FArrayBox const& av, FArrayBox const& bv, FArrayBox& cv)
     auto const& a = av.array();
     auto const& b = bv.array();
     auto const& c = cv.array();
-    amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+    amrex::LoopConcurrent(box, [&] AMREX_GPU_DEVICE (int i, int j, int k)
     {
         c(i,j,k) = a(i,j,k) + b(i,j,k);
     });
@@ -63,7 +107,7 @@ void stream_triad (FArrayBox& av, FArrayBox const& bv, FArrayBox const& cv,
     auto const& a = av.array();
     auto const& b = bv.array();
     auto const& c = cv.array();
-    amrex::ParallelFor(box, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+    amrex::LoopConcurrent(box, [&] AMREX_GPU_DEVICE (int i, int j, int k)
     {
         a(i,j,k) = b(i,j,k) + scalar*c(i,j,k);
     });
@@ -145,6 +189,8 @@ int main (int argc, char* argv[])
 
         char label[4][12] = {"Copy:      ", "Scale:     ", "Add:       ", "Triad:     "};
 
+        amrex::Print() << "\nFortran\n";
+
         for (int iproc = 0; iproc < ParallelDescriptor::NProcs(); ++iproc) {
             ParallelDescriptor::Barrier();
             if (iproc == ParallelDescriptor::MyProc()) {
@@ -159,6 +205,59 @@ int main (int argc, char* argv[])
                 std::cout << std::endl;
             }
         }
+
+#ifndef AMREX_USE_GPU
+        for (int it = 0; it < NTIMES; ++it) {
+            Real t = amrex::second();
+            stream_copy_f(faba, fabc);
+            Gpu::streamSynchronize();
+            times(0,it) = amrex::second() - t;
+
+            t = amrex::second();
+            stream_scale_f(fabb, fabc, scalar);
+            Gpu::streamSynchronize();
+            times(1,it) = amrex::second() - t;
+
+            t = amrex::second();
+            stream_add_f(faba, fabb, fabc);
+            Gpu::streamSynchronize();
+            times(2,it) = amrex::second() - t;
+
+            t = amrex::second();
+            stream_triad_f(faba, fabb, fabc, scalar);
+            Gpu::streamSynchronize();
+            times(3,it) = amrex::second() - t;
+        }
+
+        for (int j = 0; j < 4; ++j) {
+            avgtime[j] = 0.0;
+            mintime[j] = std::numeric_limits<Real>::max();
+            maxtime[j] = std::numeric_limits<Real>::lowest();
+            for (int it = 0; it < NTIMES; ++it) {
+                avgtime[j] += times(j,it);
+                mintime[j] = amrex::min(mintime[j], times(j,it));
+                maxtime[j] = amrex::max(maxtime[j], times(j,it));
+            }
+            avgtime[j] /= double(NTIMES);
+        }
+
+        amrex::Print() << "\nFortran\n";
+
+        for (int iproc = 0; iproc < ParallelDescriptor::NProcs(); ++iproc) {
+            ParallelDescriptor::Barrier();
+            if (iproc == ParallelDescriptor::MyProc()) {
+                if (ParallelDescriptor::NProcs() > 1) {
+                    std::cout << "Proc. " << iproc << std::endl;
+                }
+                std::printf("Function    Best Rate MB/s  Avg time     Min time     Max time\n");
+                for (int j = 0; j < 4; ++j) {
+                    std::printf("%s%12.1f  %11.6f  %11.6f  %11.6f\n", label[j],
+                                1.0e-06 * bytes[j]/mintime[j], avgtime[j], mintime[j], maxtime[j]);
+                }
+                std::cout << std::endl;
+            }
+        }
+#endif
     }
     amrex::Finalize();
 }
