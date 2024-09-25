a11d7eddc4ed39326c9237a9ec0dae0c9388a744
Weiqun Zhang <weiqunzhang@lbl.gov>
2024-09-25 16:50:50 -0700

    Add kokkos test

---
+++ kokkos/README.md
@@ -0,0 +1,24 @@
+This is an example of how one can build kokkos and amrex. Note that AMReX
+needs `Kokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE`
+
+* Compile Kokkos
+
+    $ cd kokkos
+    $ mkdir build
+    $ cmake .. -DCMAKE_INSTALL_PREFIX=/home/wqzhang/opt/kokkos -DKokkos_ENABLE_CUDA=ON -DKokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE=ON -DKokkos_ARCH_PASCAL60=ON
+    $ make -j8
+    $ make install
+    
+* Compile amrex
+
+    $ cd amrex
+    $ mkdir build
+    $ cmake .. -DCMAKE_INSTALL_PREFIX=/home/wqzhang/opt/amrex -DAMReX_GPU_BACKEND=CUDA -DAMReX_CUDA_ARCH=60 -DAMReX_MPI=OFF
+    $ make -j8
+    $ make install
+
+* Compile this test code
+
+    $ mkdir build
+    $ cmake .. -DENABLE_CUDA=ON -DAMReX_ROOT=/home/wqzhang/opt/amrex -DKokkos_ROOT=/home/wqzhang/opt/kokkos -DCMAKE_CUDA_ARCHITECTURES=60
+    $ make -j8
---
+++ kokkos/main.cpp
@@ -0,0 +1,74 @@
+#include <AMReX.H>
+#include <AMReX_MultiFab.H>
+#include <Kokkos_Core.hpp>
+
+using namespace amrex;
+
+static void test_amrex (MultiFab& mfa, MultiFab const& mfb)
+{
+    for (MFIter mfi(mfa); mfi.isValid(); ++mfi) {
+        const Box& bx = mfi.validbox();
+        Array4<Real const> const& b = mfb.const_array(mfi);
+        Array4<Real> const& a = mfa.array(mfi);
+        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            a(i,j,k) += 0.5*b(i,j,k);
+        });
+    }
+}
+
+static void test_kokkos (MultiFab& mfa, MultiFab const& mfb)
+{
+    for (MFIter mfi(mfa); mfi.isValid(); ++mfi) {
+        const Box& bx = mfi.validbox();
+        auto N0 = bx.length(0);
+        auto N1 = bx.length(1);
+        auto N2 = bx.length(2);
+        auto* pa = mfa[mfi].dataPtr();
+        auto const* pb = mfb[mfi].dataPtr();
+        Kokkos::View<double***,Kokkos::LayoutLeft> a(pa,N0,N1,N2);
+        Kokkos::View<double const***, Kokkos::LayoutLeft> b(pb,N0,N1,N2);
+        Kokkos::parallel_for(Kokkos::MDRangePolicy({0,0,0},{N0,N1,N2}),
+                             KOKKOS_LAMBDA (int i, int j, int k)
+        {
+            a(i,j,k) += 0.5*b(i,j,k);
+        });
+    }
+}
+
+int main (int argc, char* argv[])
+{
+    amrex::Initialize(argc, argv);
+    Kokkos::initialize(argc, argv);
+    {
+        BoxArray ba(Box(IntVect(0),IntVect(255)));
+        DistributionMapping dm(ba);
+        MultiFab mfa(ba, dm, 1, 0);
+        MultiFab mfb(ba, dm, 1, 0);
+        mfa.setVal(1.0);
+        mfb.setVal(2.0);
+        double tamrex = 1.e10;
+        double tkokkos = 1.e10;
+        for (int count = 0; count < 10; ++count) {
+            amrex::Gpu::synchronize();
+            double t0 = amrex::second();
+
+            test_amrex(mfa, mfb);
+
+            amrex::Gpu::synchronize();
+            double t1 = amrex::second();
+
+            test_kokkos(mfa, mfb);
+
+            amrex::Gpu::synchronize();
+            double t2 = amrex::second();
+
+            tamrex = std::min(t1-t0, tamrex);
+            tkokkos = std::min(t2-t1, tkokkos);
+        }
+        std::cout << "amrex  run time is " << tamrex << "\n"
+                  << "kokkos run time is " << tkokkos << "\n";
+    }
+    Kokkos::finalize();
+    amrex::Finalize();
+}
