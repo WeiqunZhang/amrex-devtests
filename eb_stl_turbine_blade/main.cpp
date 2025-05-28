6db40b101400fc857493353dfd528dbf880c20a1
Weiqun Zhang <weiqunzhang@lbl.gov>
2025-05-28 15:06:17 -0700

    multiple levels

--- eb_stl_turbine_blade/main.cpp
+++ eb_stl_turbine_blade/main.cpp
@@ -10,10 +10,11 @@ void main_main ()
 {
     BL_PROFILE("main");
 
-    int n_cell_x = 256;
-    int n_cell_y = 32;
-    int n_cell_z = 32;
+    int n_cell_x = 64;
+    int n_cell_y = 8;
+    int n_cell_z = 8;
     int max_grid_size = 32;
+    int max_level = 2;
     std::string plot_file{"plt"};
     {
         ParmParse pp;
@@ -21,6 +22,7 @@ void main_main ()
         pp.query("n_cell_y", n_cell_y);
         pp.query("n_cell_z", n_cell_z);
         pp.query("max_grid_size", max_grid_size);
+        pp.query("max_level", max_level);
         pp.query("plot_file", plot_file);
 
         ParmParse ppeb2("eb2");
@@ -30,23 +32,44 @@ void main_main ()
         ppeb2.add("stl_file", std::string("NREL_PhaseVI_blade.stl"));
     }
 
-    Geometry geom(Box(IntVect(0),IntVect(n_cell_x-1, n_cell_y-1, n_cell_z-1)),
-                  RealBox({AMREX_D_DECL( 0.  , -0.49, -0.49)},
-                          {AMREX_D_DECL( 7.68,  0.49,  0.49)}),
-                  0, {AMREX_D_DECL(0,0,0)});
-    BoxArray ba(geom.Domain());
-    ba.maxSize(max_grid_size);
-    DistributionMapping dm(ba);
-
-    {
-        BL_PROFILE("EB2:Build");
-        EB2::Build(geom, 0, 10, 1);
-	amrex::Print() << "max coarsening level is " << EB2::maxCoarseningLevel(geom) << "\n";
+    Vector<Geometry> geom;
+    for (int ilev = 0; ilev <= max_level; ++ilev) {
+        if (geom.empty()) {
+            geom.push_back(Geometry(Box(IntVect(0),IntVect(n_cell_x-1,n_cell_y-1,n_cell_z-1)),
+                                    RealBox({0.  , -0.49, -0.49},
+                                            {7.68,  0.49,  0.49}),
+                                    0, {0,0,0}));
+        } else {
+            geom.push_back(amrex::refine(geom.back(), 2));
+        }
     }
 
-    auto const& factory = makeEBFabFactory(geom, ba, dm, {1,1,1}, EBSupport::full);
-    MultiFab const& vfrc = factory->getVolFrac();
-    amrex::WriteMLMF(plot_file, {&vfrc}, {geom});
+    EB2::Build(geom.back(), max_level, max_level, 1, false);
+
+    Vector<MultiFab> mf(max_level+1);
+    for (int ilev = 0; ilev <= max_level; ++ilev) {
+        auto const& gm = geom[ilev];
+        Box bx = gm.Domain();
+        bx.grow(-ilev*2); // shrink the fine level box a little bit
+        BoxArray ba(bx);
+        ba.maxSize(max_grid_size);
+        DistributionMapping dm{ba};
+
+        auto const& factory = makeEBFabFactory(gm, ba, dm, {1,1,1}, EBSupport::full);
+        MultiCutFab const& barea = factory->getBndryArea();
+        MultiCutFab const& bnorm = factory->getBndryNormal();
+
+        int ncomp = 4; // boundary area: 1, boundary normal: 3
+        mf[ilev].define(ba,dm,ncomp,0,MFInfo{},*factory);
+        mf[ilev].ParallelCopy(barea.ToMultiFab(0.0, -1.0), 0, 0, 1);
+        mf[ilev].ParallelCopy(bnorm.ToMultiFab(-1.0, -1.0), 0, 1, 3);
+    }
+
+    // EB_WriteMultiLevelPlotfile will automatically save volume fraction
+    EB_WriteMultiLevelPlotfile("plot", max_level+1, GetVecOfConstPtrs(mf),
+                               {"barea", "nx", "ny", "nz"}, geom, 0.0,
+                               Vector<int>(max_level+1,0),
+                               Vector<IntVect>(max_level,IntVect(2)));
 }
 
 int main (int argc, char* argv[])
